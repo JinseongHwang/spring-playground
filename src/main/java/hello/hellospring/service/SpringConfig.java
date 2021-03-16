a748dd85ef9c72392d2bfd9a523fa5e26ce4e11a
JinseongHwang <eddy5360@naver.com>
2021-03-17 02:37:07 +0900

    feat: 스프링 데이터 JPA를 사용한 DB 연동

--- src/main/java/hello/hellospring/service/SpringConfig.java
+++ src/main/java/hello/hellospring/service/SpringConfig.java
@@ -35,23 +35,39 @@ public class SpringConfig {
     }
      */
 
+    /*
+    스프링 데이터 JPA를 사용하면 필요없어지는 코드들
+
     private EntityManager em;
 
     @Autowired
     public SpringConfig(EntityManager em) {
         this.em = em;
     }
+     */
+
+    private final MemberRepository memberRepository;
+
+    @Autowired // 생성자가 1개일 경우에는 생략 가능
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+//        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
+
+    /*
+    스프링 데이터 JPA를 사용하면 필요없어지는 코드들
 
     @Bean
     public MemberRepository memberRepository() {
 //        return new MemoryMemberRepository();
 //        return new JdbcMemberRepository(dataSource);
 //        return new JdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(em);
+//        return new JpaMemberRepository(em);
     }
+     */
 }
