6b2b82f54ca751450362001453b5daf5a8171686
JinseongHwang <eddy5360@naver.com>
2021-04-08 23:05:08 +0900

    feat: 정률 할인 정책으로 변경

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -2,6 +2,7 @@ package hello.core;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
@@ -25,6 +26,10 @@ public class AppConfig {
     }
 
     public DiscountPolicy discountPolicy() {
-        return new FixDiscountPolicy();
+        // 정액 할인 정책을 사용하고자 한다면 아래 코드를 활성화한다.
+//        return new FixDiscountPolicy();
+
+        // 정률 할인 정책 ..
+        return new RateDiscountPolicy();
     }
 }
