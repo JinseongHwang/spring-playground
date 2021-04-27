a19d13681b7a404ba2c6623b23c0517e136dd856
JinseongHwang <eddy5360@naver.com>
2021-04-27 23:25:23 +0900

    feat: ObjectProvider를 사용해서 Dependency Lookup(DL) 기능을 제공받음

--- src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -2,6 +2,7 @@ package hello.core.scope;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
@@ -37,12 +38,14 @@ public class SingletonWithPrototypeTest1 {
 
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
-        assertThat(count2).isEqualTo(2);
+        assertThat(count2).isEqualTo(1);
     }
 
     @Scope("singleton")
     static class ClientBean {
-        private final PrototypeBean prototypeBean;
+//        private final PrototypeBean prototypeBean;
+
+        public ObjectProvider<PrototypeBean> prototypeBeanProvider;
 
         /**
          * 싱글톤 빈이 생성과 동시에 의존관계 주입이 발생한다.
@@ -51,11 +54,18 @@ public class SingletonWithPrototypeTest1 {
          * 이 상황에서 싱글톤 빈을 계속 호출한다 하더라도 프로토타입 빈은 재생성 되지 않는다.
          */
         @Autowired
-        public ClientBean(PrototypeBean prototypeBean) {
-            this.prototypeBean = prototypeBean;
+//        public ClientBean(PrototypeBean prototypeBean) {
+//            this.prototypeBean = prototypeBean;
+//        }
+        public ClientBean(ObjectProvider<PrototypeBean> prototypeBeanProvider) {
+            this.prototypeBeanProvider = prototypeBeanProvider;
         }
 
         public int logic() {
+//            prototypeBean.addCount();
+//            return prototypeBean.getCount();
+
+            PrototypeBean prototypeBean = prototypeBeanProvider.getObject();
             prototypeBean.addCount();
             return prototypeBean.getCount();
         }
