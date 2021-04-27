ea05157cda46311fcf67cde568a76bf139d4331f
JinseongHwang <eddy5360@naver.com>
2021-04-27 14:20:27 +0900

    feat: @PostConstruct 와 @PreDestroy 어노테이션을 사용해서 초기화, 소멸

--- src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
+++ src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
@@ -18,14 +18,16 @@ public class BeanLifeCycleTest {
 
     @Configuration
     static class LifeCycleConfig {
-        /**
+        /** [Solution 02]
+         * @Bean(initMethod = "init", destroyMethod = "close")
          * 빈 설정 정보 사용 방법 특징
          * 1. 메서드 이름을 자유롭게 지정 가능하다.
          * 2. 스프링 빈이 스프링에 의존하지 않는다.
          * 3. 코드가 아니라 설정 정보를 사용하기 때문에 외부 라이브러리에도 초기화, 종료 메서드를 적용 가능하다.
          * 4. destroyMethod 속성에는 default로 "close", "shutdown"이 매칭되어 있다. default랑 같다면 생략해도 된다.
          */
-        @Bean(initMethod = "init", destroyMethod = "close")
+
+        @Bean
         public NetworkClient networkClient() {
             NetworkClient networkClient = new NetworkClient();
             networkClient.setUrl("http://hello-spring.dev");
