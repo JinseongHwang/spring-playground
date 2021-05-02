a134bc6e007a0d43742dd20f69f037e2f6531c31
JinseongHwang <eddy5360@naver.com>
2021-05-02 16:30:45 +0900

    feat: lombok으로 코드 간결화

--- ch02-jpa-start1/src/main/java/jpabook/start/Member.java
+++ ch02-jpa-start1/src/main/java/jpabook/start/Member.java
@@ -1,45 +1,26 @@
 package jpabook.start;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;  //**
 
 /**
  * User: HolyEyE
  * Date: 13. 5. 24. Time: 오후 7:43
  */
-@Entity
-@Table(name="MEMBER")
+@Getter
+@Setter
+@Entity // 이 클래스를 테이블과 매핑한다고 JPA에게 알려줌.
+@Table(name="MEMBER") // 이 테이블의 이름은 "MEMBER"이다.
 public class Member {
 
-    @Id
-    @Column(name = "ID")
+    @Id // Primary Key
+    @Column(name = "ID") // 컬럼명 = ID
     private String id;
 
-    @Column(name = "NAME")
+    @Column(name = "NAME") // 컬럼명 = NAME
     private String username;
 
-    private Integer age;
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
+    private Integer age; // 컬럼명 = age
 }
