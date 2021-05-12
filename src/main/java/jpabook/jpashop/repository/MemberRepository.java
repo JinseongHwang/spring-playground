1eccd7b6934c9121a1278e56cb689f1c45825aa3
JinseongHwang <eddy5360@naver.com>
2021-05-12 22:46:30 +0900

    feat: 회원 repository 기능 개발

---
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -0,0 +1,37 @@
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.domain.Member;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Repository
+public class MemberRepository {
+
+    @PersistenceContext
+    private EntityManager em;
+
+//    @PersistenceUnit
+//    private EntityManagerFactory emf;
+
+    public void save(Member member) {
+        em.persist(member);
+    }
+
+    public Member findOne(Long id) {
+        return em.find(Member.class, id); // (Type, PK)
+    }
+
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class) // (JPQL, return type)
+                .getResultList();
+    }
+
+    public List<Member> findByName(String name) {
+        return em.createQuery("select m from Member m where m.name=:name", Member.class)
+                .setParameter("name", name)
+                .getResultList();
+    }
+}
