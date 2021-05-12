2bb580588f2c9399d682a470c137685a54e42cad
JinseongHwang <eddy5360@naver.com>
2021-05-12 23:42:58 +0900

    feat: EntityManager가 Autowired 되도록 수정

--- src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.repository;
 
 import jpabook.jpashop.domain.Member;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
@@ -8,10 +9,11 @@ import javax.persistence.PersistenceContext;
 import java.util.List;
 
 @Repository
+@RequiredArgsConstructor
 public class MemberRepository {
 
-    @PersistenceContext
-    private EntityManager em;
+    // @PersistenceContext
+    private final EntityManager em;
 
 //    @PersistenceUnit
 //    private EntityManagerFactory emf;
