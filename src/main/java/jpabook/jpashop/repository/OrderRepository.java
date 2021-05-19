6fea6a49b78a3c2fbf49e00d1f09f38f3c39c913
JinseongHwang <eddy5360@naver.com>
2021-05-19 22:02:27 +0900

    refactor: JPA Critefia를 사용해서 동적 쿼리 생성

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -7,6 +7,8 @@ import org.springframework.util.StringUtils;
 
 import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
+import javax.persistence.criteria.*;
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
@@ -23,6 +25,9 @@ public class OrderRepository {
         return em.find(Order.class, id);
     }
 
+    /**
+     * [1] JPQL 을 문자열로 다루며 조건문으로 동적 쿼리 작성
+     */
     public List<Order> findAllByString(OrderSearch orderSearch) {
         // language=JPQL
         String jpql = "select o From Order o join o.member m";
@@ -61,6 +66,37 @@ public class OrderRepository {
             query = query.setParameter("name", orderSearch.getMemberName());
         }
 
+        return query.getResultList();
+    }
+
+    /**
+     * [2] JPA Criteria 표준 모델을 가지고 작성하는 방법
+     */
+    public List<Order> findAllByCriteria(OrderSearch orderSearch) {
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery<Order> cq = cb.createQuery(Order.class);
+        Root<Order> o = cq.from(Order.class);
+        Join<Object, Object> m = o.join("member", JoinType.INNER);
+
+        List<Predicate> criteria = new ArrayList<>();
+
+        // 주문 상태 검색
+        if (orderSearch.getOrderStatus() != null) {
+            Predicate status = cb.equal(o.get("status"), orderSearch.getOrderStatus());
+            criteria.add(status);
+        }
+
+        // 회원 상태 검색
+        if (StringUtils.hasText(orderSearch.getMemberName())) {
+            Predicate name = cb.like(
+                    m.<String>get("name"),
+                    "%" + orderSearch.getMemberName() + "%"
+            );
+            criteria.add(name);
+        }
+
+        cq.where(cb.and(criteria.toArray(new Predicate[criteria.size()])));
+        TypedQuery<Order> query = em.createQuery(cq).setMaxResults(1000);
         return query.getResultList();
     }
 
