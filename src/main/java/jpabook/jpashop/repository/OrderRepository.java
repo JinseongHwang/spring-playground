4457aec0492c77e3b2d17d3e622adf719d8b3bda
JinseongHwang <eddy5360@naver.com>
2021-05-19 21:19:17 +0900

    feat: 주문 검색 기능 정적 쿼리 형태로 작성
    지금은 MemberStatus 와 MemberName 이 둘 다 입력된다는 가정 하에 정상적으로 동작하는 코드이다. 하지만 어떤 값이 입력되고 입력 안될지 모르기 때문에 동적 쿼리 형태로 변경이 필요하다.

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
+import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
@@ -20,5 +21,13 @@ public class OrderRepository {
         return em.find(Order.class, id);
     }
 
-//    public List<Order> findAll(OrderSearch orderSearch) {}
+    public List<Order> findAll(OrderSearch orderSearch) {
+        return em.createQuery("select o from Order o join o.member m " +
+                "where o.status = :status " +
+                "and m.name like :name", Order.class)
+                .setParameter("status", orderSearch.getOrderStatus())
+                .setParameter("name", orderSearch.getMemberName())
+                .setMaxResults(1000) // 최대 1000건
+                .getResultList();
+    }
 }
