0bd48c8ed6a4017006734e0cfb1080fb83560e3b
JinseongHwang <eddy5360@naver.com>
2021-05-30 15:31:24 +0900

    feat: dirty checking으로 상품 수정 기능 구현

--- src/main/java/jpabook/jpashop/service/ItemService.java
+++ src/main/java/jpabook/jpashop/service/ItemService.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.service;
 
+import jpabook.jpashop.domain.item.Book;
 import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
@@ -18,6 +19,14 @@ public class ItemService {
     @Transactional
     public void saveItem(Item item) {
         itemRepository.save(item);
+    }
+
+    @Transactional
+    public void updateItem(Long itemId, Book bookParam) {
+        Item findItem = itemRepository.findOne(itemId);
+        findItem.setPrice(bookParam.getPrice());
+        findItem.setName(bookParam.getName());
+        findItem.setStockQuantity(bookParam.getStockQuantity());
     }
 
     public List<Item> findItems() {
---
+++ src/test/java/jpabook/jpashop/service/ItemUpdateTest.java
@@ -0,0 +1,30 @@
+package jpabook.jpashop.service;
+
+import javax.persistence.EntityManager;
+import jpabook.jpashop.domain.item.Book;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ItemUpdateTest {
+
+    @Autowired
+    EntityManager em;
+
+    @Test
+    public void updateTest() throws Exception {
+        // given
+        Book book = em.find(Book.class, 1L);
+
+        // when
+        // 변경 감지 -> Dirty checking
+        book.setName("asdf");
+
+        // then
+
+    }
+}
