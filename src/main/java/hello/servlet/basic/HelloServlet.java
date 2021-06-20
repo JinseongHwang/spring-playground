2512cfb38edff358745cbdd2d334b9b910ec9283
JinseongHwang <eddy5360@naver.com>
2021-06-20 16:25:48 +0900

    feat: 연습용 서블릿 생성

---
+++ src/main/java/hello/servlet/basic/HelloServlet.java
@@ -0,0 +1,27 @@
+package hello.servlet.basic;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "helloServlet", urlPatterns = "/hello")
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException {
+        System.out.println("HelloServlet.service");
+        System.out.println("request = " + request);
+        System.out.println("response = " + response);
+
+        String username = request.getParameter("username");
+        System.out.println("username = " + username);
+
+        response.setContentType("text/plain"); // response header
+        response.setCharacterEncoding("utf-8"); // response header
+        response.getWriter().write("hello " + username); // HTTP Message Body write
+    }
+}
