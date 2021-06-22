29a57a591002ea0e2a74c0c50390ab70bd8d2b4c
JinseongHwang <eddy5360@naver.com>
2021-06-23 01:54:22 +0900

    feat: model이 있는 렌더링 함수 오버로딩

--- src/main/java/hello/servlet/web/frontcontroller/MyView.java
+++ src/main/java/hello/servlet/web/frontcontroller/MyView.java
@@ -1,6 +1,7 @@
 package hello.servlet.web.frontcontroller;
 
 import java.io.IOException;
+import java.util.Map;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -19,5 +20,19 @@ public class MyView {
 
         RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
         dispatcher.forward(request, response);
+    }
+
+    public void render(Map<String, Object> model, HttpServletRequest request,
+        HttpServletResponse response) throws ServletException, IOException {
+
+        modelToRequestAttribute(model, request);
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+        dispatcher.forward(request, response);
+    }
+
+    private void modelToRequestAttribute(Map<String, Object> model, HttpServletRequest request) {
+        // JSP 에서 값들을 꺼내쓰기 위해 model 의 모든 값을 request 객체에 저장해준다.
+        // model.forEach((key, value) -> request.setAttribute(key, value)); -> 간소화
+        model.forEach(request::setAttribute);
     }
 }
