032395b09f3a6cbd8098e0393eec2db9e301f179
JinseongHwang <eddy5360@naver.com>
2021-06-23 14:11:12 +0900

    feat: model을 추가하고 사용자는 view의 논리 이름만 반환하면 됨

---
+++ src/main/java/hello/servlet/web/frontcontroller/v4/ControllerV4.java
@@ -0,0 +1,14 @@
+package hello.servlet.web.frontcontroller.v4;
+
+import java.util.Map;
+
+public interface ControllerV4 {
+
+    /**
+     * @param paramMap
+     * @param model
+     * @return viewName
+     */
+    String process(Map<String, String> paramMap, Map<String, Object> model);
+
+}
---
+++ src/main/java/hello/servlet/web/frontcontroller/v4/controller/MemberSaveControllerV4.java
@@ -0,0 +1,23 @@
+package hello.servlet.web.frontcontroller.v4.controller;
+
+import hello.servlet.domain.member.Member;
+import hello.servlet.domain.member.MemberRepository;
+import hello.servlet.web.frontcontroller.v4.ControllerV4;
+import java.util.Map;
+
+public class MemberSaveControllerV4 implements ControllerV4 {
+
+    private final MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    public String process(Map<String, String> paramMap, Map<String, Object> model) {
+        String username = paramMap.get("username");
+        int age = Integer.parseInt(paramMap.get("age"));
+
+        Member member = new Member(username, age);
+        memberRepository.save(member);
+
+        model.put("member", member); // 모델에 새로운 회원 정보를 저장
+        return "save-result"; // JSP 파일의 논리 이름만 반환
+    }
+}
