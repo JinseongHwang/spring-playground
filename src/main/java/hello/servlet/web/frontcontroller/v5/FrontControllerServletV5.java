8eec114c11154473f8a77a3c112e7f509a15a3ab
JinseongHwang <eddy5360@naver.com>
2021-06-27 18:34:11 +0900

    feat: 동작 방식에 대한 주석 추가

--- src/main/java/hello/servlet/web/frontcontroller/v5/FrontControllerServletV5.java
+++ src/main/java/hello/servlet/web/frontcontroller/v5/FrontControllerServletV5.java
@@ -53,21 +53,31 @@ public class FrontControllerServletV5 extends HttpServlet {
     protected void service(HttpServletRequest request, HttpServletResponse response)
         throws ServletException, IOException {
 
+        // 1. 접속한 URI 를 보고 처리 가능한 핸들러를 조회해서 반환한다.
         Object handler = getHandler(request);
-
-        if (handler == null) { // 404
+        if (handler == null) { // 404: 적절한 핸들러를 찾지 못한 경우, 유효하지 않은 접근
             response.setStatus(HttpServletResponse.SC_NOT_FOUND);
             return;
         }
 
+        // 2. 핸들러를 처리할 수 있는 어댑터를 조회해서 반환한다. 없다면 예외 발생
         MyHandlerAdapter adapter = getHandlerAdapter(handler);
 
-        ModelView mv = adapter.handle(request, response, handler);
+        // 3. support 메서드를 통과한 핸들러만 handle() 함수에 접근할 수 있다.
+        // 또한 그 핸들러는 인터페이스가 아닌 구체 클래스이므로 process 메서드를 호출할 수 있다.
+        ModelView mv = adapter.handle(request, response, handler); // ModelView 객체 반환
 
+        // 4. JSP 파일 이름을 실제 경로로 변환
         String viewName = mv.getViewName(); // 논리 이름
         MyView view = viewResolver(viewName);
 
+        // 5. 모델 값을 참조하여 렌더링
         view.render(mv.getModel(), request, response);
+    }
+
+    private Object getHandler(HttpServletRequest request) {
+        String requestURI = request.getRequestURI();
+        return handlerMappingMap.get(requestURI);
     }
 
     private MyHandlerAdapter getHandlerAdapter(Object handler) {
@@ -77,11 +87,6 @@ public class FrontControllerServletV5 extends HttpServlet {
             }
         }
         throw new IllegalArgumentException("handler adapter not found. handler = " + handler);
-    }
-
-    private Object getHandler(HttpServletRequest request) {
-        String requestURI = request.getRequestURI();
-        return handlerMappingMap.get(requestURI);
     }
 
     private MyView viewResolver(String viewName) {
