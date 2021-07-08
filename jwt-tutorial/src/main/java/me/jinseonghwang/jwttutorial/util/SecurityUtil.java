e8e7d54234e11b2bd70a224e72946799c4d8567e
JinseongHwang <eddy5360@naver.com>
2021-07-09 04:22:28 +0900

    feat: SecurityContext 의 인증 정보에서 username 을 반환

---
+++ jwt-tutorial/src/main/java/me/jinseonghwang/jwttutorial/util/SecurityUtil.java
@@ -0,0 +1,40 @@
+package me.jinseonghwang.jwttutorial.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Optional;
+
+public class SecurityUtil {
+
+    private static final Logger logger = LoggerFactory.getLogger(SecurityUtil.class);
+
+    private SecurityUtil() {
+    }
+
+    /**
+     * JwtFilter 클래스의 doFilter 메서드에서 저장한 SecurityContext 의 인증 정보에서 username 을 반환한다.
+     */
+    public static Optional<String> getCurrentUsername() {
+        // SecurityContext 에서 Authentication 객체를 꺼내온다.
+        final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication == null) {
+            logger.debug("Security Context에 인증 정보가 없습니다.");
+            return Optional.empty();
+        }
+
+        String username = null;
+        if (authentication.getPrincipal() instanceof UserDetails) {
+            UserDetails springSecurityUser = (UserDetails) authentication.getPrincipal();
+            username = springSecurityUser.getUsername();
+        } else if (authentication.getPrincipal() instanceof String) {
+            username = (String) authentication.getPrincipal();
+        }
+
+        return Optional.ofNullable(username);
+    }
+}
