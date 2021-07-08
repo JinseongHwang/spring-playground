91a242c509790d0bd5449df0dc1ecb68e61bcd0a
JinseongHwang <eddy5360@naver.com>
2021-07-09 00:03:13 +0900

    feat: 사용자, 인증 엔티티 생성

---
+++ jwt-tutorial/src/main/java/me/jinseonghwang/jwttutorial/entity/Authority.java
@@ -0,0 +1,18 @@
+package me.jinseonghwang.jwttutorial.entity;
+
+import lombok.*;
+import javax.persistence.*;
+
+@Entity
+@Table(name = "authority")
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Authority {
+
+    @Id
+    @Column(name = "authority_name", length = 50)
+    private String authorityName;
+}
