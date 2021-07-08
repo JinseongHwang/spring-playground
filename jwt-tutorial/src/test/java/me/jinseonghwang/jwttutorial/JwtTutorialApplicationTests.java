cb1d777427d4414e3b64c621a6b5a1d243186a65
JinseongHwang <eddy5360@naver.com>
2021-07-08 23:22:44 +0900

    feat: 프로젝트 생성

---
+++ jwt-tutorial/src/test/java/me/jinseonghwang/jwttutorial/JwtTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package me.jinseonghwang.jwttutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
