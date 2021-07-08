efe890be6e962227409e077f51e1b67125b09e14
JinseongHwang <eddy5360@naver.com>
2021-07-09 04:21:51 +0900

    feat: DB 에서 조회한 User 및 권한 정보들을 userdetails.User 객체로 변환하여 반환

---
+++ jwt-tutorial/src/main/java/me/jinseonghwang/jwttutorial/service/CustomUserDetailsService.java
@@ -0,0 +1,47 @@
+package me.jinseonghwang.jwttutorial.service;
+
+import me.jinseonghwang.jwttutorial.entity.User;
+import me.jinseonghwang.jwttutorial.repository.UserRepository;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component("userDetailsService")
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    /**
+     * DB 에서 조회한 User 및 권한 정보들을 userdetails.User 객체로 변환하여 반환한다.
+     */
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(final String username) {
+        return userRepository.findOneWithAuthoritiesByUsername(username)
+            .map(user -> createUser(username, user))
+            .orElseThrow(() -> new UsernameNotFoundException(username + " -> 데이터베이스에서 찾을 수 없습니다."));
+    }
+
+    private org.springframework.security.core.userdetails.User createUser(String username, User user) {
+        if (!user.isActivated()) {
+            throw new RuntimeException(username + " -> 활성화되어 있지 않습니다.");
+        }
+        List<GrantedAuthority> grantedAuthorities = user.getAuthorities().stream()
+            .map(authority -> new SimpleGrantedAuthority(authority.getAuthorityName()))
+            .collect(Collectors.toList());
+        return new org.springframework.security.core.userdetails.User(user.getUsername(),
+            user.getPassword(),
+            grantedAuthorities);
+    }
+}
