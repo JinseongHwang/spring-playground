f321fc0de15a65b6b77805dffbb6a66f9ba59b2d
JinseongHwang <eddy5360@naver.com>
2021-07-09 22:51:51 +0900

    chore: 프로젝트 생성

---
+++ restful-jwt-board/src/main/java/me/jinseonghwang/restfuljwtboard/RestfulJwtBoardApplication.java
@@ -0,0 +1,13 @@
+package me.jinseonghwang.restfuljwtboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulJwtBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulJwtBoardApplication.class, args);
+	}
+
+}
---
+++ restful-jwt-board/src/test/java/me/jinseonghwang/restfuljwtboard/RestfulJwtBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package me.jinseonghwang.restfuljwtboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulJwtBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
