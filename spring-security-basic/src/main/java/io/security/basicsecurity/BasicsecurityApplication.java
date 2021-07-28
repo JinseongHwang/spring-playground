21909f49d5c15cfb5e48c86565c8ed6d32ab779d
JinseongHwang <eddy5360@naver.com>
2021-07-28 17:18:04 +0900

    chore: 프로젝트 생성

---
+++ spring-security-basic/src/main/java/io/security/basicsecurity/BasicsecurityApplication.java
@@ -0,0 +1,13 @@
+package io.security.basicsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicsecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicsecurityApplication.class, args);
+    }
+
+}
---
+++ spring-security-basic/src/test/java/io/security/basicsecurity/BasicsecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package io.security.basicsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicsecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
