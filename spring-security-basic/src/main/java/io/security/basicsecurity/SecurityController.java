07911fe54df38e39b7e8093fd4d904f20ca97308
JinseongHwang <eddy5360@naver.com>
2021-08-08 17:20:15 +0900

    feat: Spring Security 의존성 추가

---
+++ spring-security-basic/src/main/java/io/security/basicsecurity/SecurityController.java
@@ -0,0 +1,13 @@
+package io.security.basicsecurity;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SecurityController {
+
+    @GetMapping("/")
+    public String index() {
+        return "home";
+    }
+}
