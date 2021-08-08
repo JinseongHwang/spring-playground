02c87025e5c1f46b05248699aaa97f3fa7423fae
JinseongHwang <eddy5360@naver.com>
2021-08-08 19:15:49 +0900

    feat: form login practice

--- spring-security-basic/src/main/java/io/security/basicsecurity/SecurityConfig.java
+++ spring-security-basic/src/main/java/io/security/basicsecurity/SecurityConfig.java
@@ -1,9 +1,17 @@
 package io.security.basicsecurity;
 
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 @Configuration
 @EnableWebSecurity // 보안 관련 여러 클래스들을 import 해서 실행하는 어노테이션
@@ -16,6 +24,31 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             .anyRequest().authenticated();
 
         http // 인증 정책
-            .formLogin();
+            .formLogin()
+            // .loginPage("/loginPage")
+            .defaultSuccessUrl("/")
+            .failureUrl("/login")
+            .usernameParameter("userId")
+            .passwordParameter("passwd")
+            .loginProcessingUrl("/login_proc")
+            .successHandler(new AuthenticationSuccessHandler() {
+                @Override
+                public void onAuthenticationSuccess(HttpServletRequest httpServletRequest,
+                    HttpServletResponse httpServletResponse, Authentication authentication)
+                    throws IOException, ServletException {
+                    System.out.println("authentication = " + authentication.getName());
+                    httpServletResponse.sendRedirect("/");
+                }
+            })
+            .failureHandler(new AuthenticationFailureHandler() {
+                @Override
+                public void onAuthenticationFailure(HttpServletRequest httpServletRequest,
+                    HttpServletResponse httpServletResponse, AuthenticationException e)
+                    throws IOException, ServletException {
+                    System.out.println("exception = " + e.getMessage());
+                    httpServletResponse.sendRedirect("/login");
+                }
+            })
+            .permitAll();
     }
 }
--- spring-security-basic/src/main/java/io/security/basicsecurity/SecurityController.java
+++ spring-security-basic/src/main/java/io/security/basicsecurity/SecurityController.java
@@ -10,4 +10,9 @@ public class SecurityController {
     public String index() {
         return "home";
     }
+
+    @GetMapping("/loginPage")
+    public String loginPage() {
+        return "loginPage";
+    }
 }
