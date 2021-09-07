744bda92965ea28ef1e8d6b1f561fb4fa2cb8ac7
JinseongHwang <eddy5360@naver.com>
2021-09-07 12:17:59 +0900

    feat: 프로젝트 생성

---
+++ spring-batch/src/main/java/me/study/springbatch/SpringBatchApplication.java
@@ -0,0 +1,14 @@
+package me.study.springbatch;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBatchApplication.class, args);
+    }
+
+}
