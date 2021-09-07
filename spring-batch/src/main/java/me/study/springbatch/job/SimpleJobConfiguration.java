6b84f4c6a14c7ed72960b7dcc63ddaef72776465
JinseongHwang <eddy5360@naver.com>
2021-09-07 15:32:59 +0900

    feat: job, step 실습

--- spring-batch/src/main/java/me/study/springbatch/job/SimpleJobConfiguration.java
+++ spring-batch/src/main/java/me/study/springbatch/job/SimpleJobConfiguration.java
@@ -5,8 +5,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -21,17 +23,32 @@ public class SimpleJobConfiguration {
     @Bean
     public Job simpleJob() {
         return jobBuilderFactory.get("simpleJob") // simpleJob 이라는 이름의 Job을 생성
-            .start(simpleStep1())
+            .start(simpleStep1(null))
+            .next(simpleStep2(null))
             .build();
     }
 
-    private Step simpleStep1() {
+    @Bean
+    @JobScope
+    public Step simpleStep1(@Value("#{jobParameters[requestDate]}") String requestDate) {
         return stepBuilderFactory.get("simpleStep1") // simpleStep1 이라는 이름의 Batch step 생성
             .tasklet((contribution, chunkContext) -> { // step 안에서 단일로 수행될 기능을 명시
                 log.info(">>>>> This is Step1");
+                log.info(">>>>> requestDate = {}", requestDate);
                 return RepeatStatus.FINISHED;
             })
             .build();
     }
 
+    @Bean
+    @JobScope
+    public Step simpleStep2(@Value("#{jobParameters[requestDate]}") String requestDate) {
+        return stepBuilderFactory.get("simpleStep2") // simpleStep2 이라는 이름의 Batch step 생성
+            .tasklet((contribution, chunkContext) -> { // step 안에서 단일로 수행될 기능을 명시
+                log.info(">>>>> This is Step2");
+                log.info(">>>>> requestDate = {}", requestDate);
+                return RepeatStatus.FINISHED;
+            })
+            .build();
+    }
 }
