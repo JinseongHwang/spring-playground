1cab47b50c65bbc2202caed80c58a5a8ba426b42
JinseongHwang <eddy5360@naver.com>
2021-09-13 15:05:43 +0900

    docs: 로그인 과정 주석 추가

--- spring-security-basic/src/main/java/io/security/basicsecurity/SecurityConfig.java
+++ spring-security-basic/src/main/java/io/security/basicsecurity/SecurityConfig.java
@@ -15,7 +15,7 @@ import org.springframework.security.web.authentication.AuthenticationSuccessHand
 
 @Configuration
 @EnableWebSecurity // 보안 관련 여러 클래스들을 import 해서 실행하는 어노테이션
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig extends WebSecurityConfigurerAdapter { // 상속 필수
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -25,13 +25,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
         http // 인증 정책
             .formLogin()
-            // .loginPage("/loginPage")
-            .defaultSuccessUrl("/")
-            .failureUrl("/login")
-            .usernameParameter("userId")
-            .passwordParameter("passwd")
-            .loginProcessingUrl("/login_proc")
-            .successHandler(new AuthenticationSuccessHandler() {
+//            .loginPage("/loginPage") // 사용자 정의 로그인 페이지
+            .defaultSuccessUrl("/") // 로그인 성공 후 이동 페이지
+            .failureUrl("/login") // 로그인 실패 후 이동 페이지
+            .usernameParameter("userId") // 아이디 파라미터명
+            .passwordParameter("passwd") // 패스워드 파라미터명
+            .loginProcessingUrl("/login_proc") // 로그인 폼 Action url
+            .successHandler(new AuthenticationSuccessHandler() { // 로그인 성공 후 핸들러
                 @Override
                 public void onAuthenticationSuccess(HttpServletRequest httpServletRequest,
                     HttpServletResponse httpServletResponse, Authentication authentication)
@@ -40,7 +40,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                     httpServletResponse.sendRedirect("/");
                 }
             })
-            .failureHandler(new AuthenticationFailureHandler() {
+            .failureHandler(new AuthenticationFailureHandler() { // 로그인 실패 후 핸들러
                 @Override
                 public void onAuthenticationFailure(HttpServletRequest httpServletRequest,
                     HttpServletResponse httpServletResponse, AuthenticationException e)
