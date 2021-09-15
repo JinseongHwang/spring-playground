03b6e691a2df5b99407f025b40f259e7a2f22ff6
jinseonghwang <jinseong.dev@gmail.com>
2021-09-15 10:43:53 +0900

    feat: 시큐리티 예외 처리

--- spring-security-basic/src/main/java/io/security/basicsecurity/SecurityController.java
+++ spring-security-basic/src/main/java/io/security/basicsecurity/SecurityController.java
@@ -30,4 +30,14 @@ public class SecurityController {
     public String admin() {
         return "admin";
     }
+
+    @GetMapping("/denied")
+    public String denied() {
+        return "Access is denied.";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
 }
--- spring-security-basic/src/main/java/io/security/basicsecurity/UserSecurityConfig.java
+++ spring-security-basic/src/main/java/io/security/basicsecurity/UserSecurityConfig.java
@@ -1,11 +1,24 @@
 package io.security.basicsecurity;
 
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
+import org.springframework.security.web.savedrequest.RequestCache;
+import org.springframework.security.web.savedrequest.SavedRequest;
 
 @Order(1)
 @Configuration
@@ -26,12 +39,40 @@ public class UserSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
             .authorizeRequests()
+            .antMatchers("/login").permitAll()
             .antMatchers("/user").hasRole("USER")
             .antMatchers("/admin/pay").hasRole("ADMIN")
             .antMatchers("/admin/**").access("hasRole('ADMIN') or hasRole('SYS')")
             .anyRequest().authenticated();
 
         http
-            .formLogin();
+            .formLogin()
+            .successHandler(new AuthenticationSuccessHandler() {
+                @Override
+                public void onAuthenticationSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse,
+                    Authentication authentication) throws IOException, ServletException {
+                    RequestCache requestCache = new HttpSessionRequestCache();
+                    SavedRequest savedRequest = requestCache.getRequest(httpServletRequest, httpServletResponse);
+                    String redirectUrl = savedRequest.getRedirectUrl();
+                    httpServletResponse.sendRedirect(redirectUrl);
+                }
+            });
+
+        http
+            .exceptionHandling() // 예외 처리 기능 동작
+            .authenticationEntryPoint(new AuthenticationEntryPoint() { // 인증 실패 시 처리
+                @Override
+                public void commence(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AuthenticationException e)
+                    throws IOException, ServletException {
+                    httpServletResponse.sendRedirect("/login");
+                }
+            })
+            .accessDeniedHandler(new AccessDeniedHandler() { // 인가 실패 시 처리
+                @Override
+                public void handle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AccessDeniedException e)
+                    throws IOException, ServletException {
+                    httpServletResponse.sendRedirect("/denied");
+                }
+            });
     }
 }
