3b60c7d05f3ff08662e1755bee550a75745ec1ec
Jinseong Hwang <jinseong.dev@gmail.com>
2021-09-18 18:31:57 +0900

    feat: 프로젝트 생성

---
+++ jpa-basic/src/main/java/me/study/jpabasic/JpaBasicApplication.java
@@ -0,0 +1,13 @@
+package me.study.jpabasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaBasicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaBasicApplication.class, args);
+    }
+
+}
---
+++ jpa-basic/src/test/java/me/study/jpabasic/JpaBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package me.study.jpabasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaBasicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
