c5afada74c42d0f00e640685b59f69cfe07fd662
Jinseong Hwang <jinseong.dev@gmail.com>
2021-10-21 19:45:58 +0900

    feat: 프로젝트 생성

---
+++ java-test/src/main/java/me/study/javatest/JavaTestApplication.java
@@ -0,0 +1,13 @@
+package me.study.javatest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaTestApplication.class, args);
+    }
+
+}
---
+++ java-test/src/test/java/me/study/javatest/JavaTestApplicationTests.java
@@ -0,0 +1,13 @@
+package me.study.javatest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
