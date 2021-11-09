c36c00541b6f00eb13ecf68bcd6bc7e2b0fcd0ef
Jinseong Hwang <jinseong.dev@gmail.com>
2021-11-09 16:39:36 +0900

    chore: 프로젝트 생성

---
+++ spring-batch-jojoldu/src/main/java/me/study/springbatchjojoldu/SpringBatchJojolduApplication.java
@@ -0,0 +1,13 @@
+package me.study.springbatchjojoldu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchJojolduApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBatchJojolduApplication.class, args);
+    }
+
+}
---
+++ spring-batch-jojoldu/src/test/java/me/study/springbatchjojoldu/SpringBatchJojolduApplicationTests.java
@@ -0,0 +1,13 @@
+package me.study.springbatchjojoldu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchJojolduApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
