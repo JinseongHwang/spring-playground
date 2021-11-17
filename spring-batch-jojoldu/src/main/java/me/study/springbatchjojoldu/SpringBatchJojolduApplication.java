c5f37eff59bd335f390f653e249bb8b4cd8e8c39
Jinseong Hwang <jinseong.dev@gmail.com>
2021-11-17 16:23:31 +0900

    feat: 배치 설정

--- spring-batch-jojoldu/src/main/java/me/study/springbatchjojoldu/SpringBatchJojolduApplication.java
+++ spring-batch-jojoldu/src/main/java/me/study/springbatchjojoldu/SpringBatchJojolduApplication.java
@@ -1,8 +1,10 @@
 package me.study.springbatchjojoldu;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableBatchProcessing // 배치 기능 활성화
 @SpringBootApplication
 public class SpringBatchJojolduApplication {
 
