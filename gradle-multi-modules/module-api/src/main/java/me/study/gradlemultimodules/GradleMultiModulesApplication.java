ae9f50716c8fc08495c72a5b49e9c67aee4f5133
Jinseong Hwang <jinseong.dev@gmail.com>
2021-12-23 18:17:57 +0900

    chore: 멀티모듈 프로젝트 생성

---
+++ gradle-multi-modules/module-api/src/main/java/me/study/gradlemultimodules/GradleMultiModulesApplication.java
@@ -0,0 +1,13 @@
+package me.study.gradlemultimodules;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleMultiModulesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GradleMultiModulesApplication.class, args);
+    }
+
+}
