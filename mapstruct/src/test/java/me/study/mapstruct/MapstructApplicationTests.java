65b02ccfa6edf31aa45e403b2b9c39dff13f6fb6
Jinseong Hwang <jinseong.dev@gmail.com>
2022-01-18 17:13:49 +0900

    chore: 프로젝트 생성

---
+++ mapstruct/src/test/java/me/study/mapstruct/MapstructApplicationTests.java
@@ -0,0 +1,13 @@
+package me.study.mapstruct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapstructApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
