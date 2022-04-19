84a6a78d3af3c087defebe2180c35e60f24bf9cd
Jinseong Hwang <jinseong.dev@gmail.com>
2022-04-19 11:33:01 +0900

    chore: init project

---
+++ jpa-specifications/src/main/java/me/study/jpaspecifications/JpaSpecificationsApplication.java
@@ -0,0 +1,13 @@
+package me.study.jpaspecifications;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaSpecificationsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaSpecificationsApplication.class, args);
+    }
+
+}
---
+++ jpa-specifications/src/test/java/me/study/jpaspecifications/JpaSpecificationsApplicationTests.java
@@ -0,0 +1,13 @@
+package me.study.jpaspecifications;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaSpecificationsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
