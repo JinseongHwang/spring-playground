03f7e3109222d51d2812aeb253e0738def9153fe
Jinseong Hwang <jinseong.dev@gmail.com>
2022-04-24 15:45:28 +0900

    chore: 프로젝트 생성

---
+++ spring-cloud-sleuth/README.md
@@ -0,0 +1,8 @@
+# Spring Cloud Sleuth
+
+아직 못함.
+
+## Reference
+
+- https://docs.spring.io/spring-cloud-sleuth/docs/3.0.5/reference/html/
+- https://github.com/fightnyy/Spring-Cloud-Sleuth-Example
---
+++ spring-cloud-sleuth/src/test/java/me/study/springcloudsleuth/SpringCloudSleuthApplicationTests.java
@@ -0,0 +1,13 @@
+package me.study.springcloudsleuth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudSleuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
