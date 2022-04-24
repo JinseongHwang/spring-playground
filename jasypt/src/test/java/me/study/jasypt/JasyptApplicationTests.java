e6a17831a930f6b701aaad7bb1606787fc3b116d
Jinseong Hwang <jinseong.dev@gmail.com>
2022-04-24 13:10:51 +0900

    chore: 프로젝트 생성

---
+++ jasypt/src/test/java/me/study/jasypt/JasyptApplicationTests.java
@@ -0,0 +1,13 @@
+package me.study.jasypt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JasyptApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
