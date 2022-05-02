4fcaba21504a04c691e8db823ee533debd022cb5
Jinseong Hwang <jinseong.dev@gmail.com>
2022-05-02 22:37:45 +0900

    chore: init project

---
+++ thymeleaf-basic/src/main/java/me/study/thymeleaf/ThymeleafBasicApplication.java
@@ -0,0 +1,13 @@
+package me.study.thymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafBasicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThymeleafBasicApplication.class, args);
+    }
+
+}
---
+++ thymeleaf-basic/src/test/java/me/study/thymeleaf/ThymeleafBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package me.study.thymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafBasicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
