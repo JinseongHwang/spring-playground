aa60a538da2b3d8dab70fd9848faf0635c3e5c5e
Jinseong Hwang <jinseong.dev@gmail.com>
2022-05-02 23:32:23 +0900

    feat: text, utext 실습

---
+++ thymeleaf-basic/src/main/java/me/study/thymeleaf/basic/BasicController.java
@@ -0,0 +1,23 @@
+package me.study.thymeleaf.basic;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/basic")
+public class BasicController {
+
+    @GetMapping("/text-basic")
+    public String textBasic(Model model) {
+        model.addAttribute("data", "Hello Spring!");
+        return "basic/text-basic";
+    }
+
+    @GetMapping("/text-unescaped")
+    public String textUnescaped(Model model) {
+        model.addAttribute("data", "Hello <b>Spring!</b>");
+        return "basic/text-unescaped";
+    }
+}
