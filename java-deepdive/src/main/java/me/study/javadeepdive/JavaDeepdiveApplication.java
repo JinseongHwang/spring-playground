79b6836d0ccc1e4782d5f3d167033a1b62953925
Jinseong Hwang <jinseong.dev@gmail.com>
2022-05-08 23:25:04 +0900

    chore: 프로젝트 생성

---
+++ java-deepdive/src/main/java/me/study/javadeepdive/JavaDeepdiveApplication.java
@@ -0,0 +1,13 @@
+package me.study.javadeepdive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDeepdiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaDeepdiveApplication.class, args);
+	}
+
+}
---
+++ java-deepdive/src/test/java/me/study/javadeepdive/JavaDeepdiveApplicationTests.java
@@ -0,0 +1,13 @@
+package me.study.javadeepdive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaDeepdiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
