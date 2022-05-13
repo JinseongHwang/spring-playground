50466a0c619723dfefb9b0b2cc365f38569a298a
Jinseong Hwang <jinseong.dev@gmail.com>
2022-05-14 01:30:30 +0900

    feat: implement eureka server

---
+++ msa/discoveryservice/src/test/java/me/study/msa/discoveryservice/DiscoveryserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package me.study.msa.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
