f42417b9f9719458823add0833823b9aa3a47105
Jinseong Hwang <jinseong.dev@gmail.com>
2022-05-18 23:21:37 +0900

    feat: implement spring cloud api gateway

---
+++ msa/api-gateway-service/src/test/java/me/study/mas/apigatewayservice/ApiGatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package me.study.mas.apigatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
