a683ef990860d45de7fe217dce880bd9ba2f15a4
Jinseong Hwang <jinseong.dev@gmail.com>
2022-05-19 01:13:12 +0900

    feat: add gateway filter

--- msa/first-service/src/main/java/me/study/msa/firstservice/FirstServiceController.java
+++ msa/first-service/src/main/java/me/study/msa/firstservice/FirstServiceController.java
@@ -1,9 +1,12 @@
 package me.study.msa.firstservice;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RequestMapping("/first-service")
 @RestController
 public class FirstServiceController {
@@ -11,5 +14,11 @@ public class FirstServiceController {
     @GetMapping("/welcome")
     public String welcome() {
         return "Welcome to the First service.";
+    }
+
+    @GetMapping("/message")
+    public String message(@RequestHeader("first-request") String header) {
+        log.info("header = {}", header);
+        return "Hello world in First Service";
     }
 }
--- msa/second-service/src/main/java/me/study/msa/secondservice/SecondServiceController.java
+++ msa/second-service/src/main/java/me/study/msa/secondservice/SecondServiceController.java
@@ -1,9 +1,12 @@
 package me.study.msa.secondservice;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RequestMapping("/second-service")
 @RestController
 public class SecondServiceController {
@@ -11,5 +14,12 @@ public class SecondServiceController {
     @GetMapping("/welcome")
     public String welcome() {
         return "Welcome to the Second service.";
+    }
+
+
+    @GetMapping("/message")
+    public String message(@RequestHeader("second-request") String header) {
+        log.info("header = {}", header);
+        return "Hello world in Second Service";
     }
 }
