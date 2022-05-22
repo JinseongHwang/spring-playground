caea2514bf96b9a305b191de69672af05d24ed80
Jinseong Hwang <jinseong.dev@gmail.com>
2022-05-22 21:45:36 +0900

    feat: register test servers to eureka server with random port mapping

--- msa/second-service/src/main/java/me/study/msa/secondservice/SecondServiceController.java
+++ msa/second-service/src/main/java/me/study/msa/secondservice/SecondServiceController.java
@@ -1,15 +1,21 @@
 package me.study.msa.secondservice;
 
+import javax.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
+@RequiredArgsConstructor
 @RequestMapping("/second-service")
 @RestController
 public class SecondServiceController {
+
+    private final Environment env;
 
     @GetMapping("/welcome")
     public String welcome() {
@@ -24,7 +30,9 @@ public class SecondServiceController {
     }
 
     @GetMapping("/check")
-    public String check() {
-        return "Hi there, This is a message from Second Service";
+    public String check(HttpServletRequest request) {
+        log.info("Server port = {}", request.getServerPort());
+        return String.format("Hi there, This is a message from Second Service on PORT %s",
+                             env.getProperty("local.server.port"));
     }
 }
