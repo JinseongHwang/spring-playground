9f5d2faad0ad68e1b900044133674f8c828d0094
Jinseong Hwang <jinseong.dev@gmail.com>
2022-06-03 02:34:07 +0900

    feat: create grpc server

--- java-grpc/src/main/java/ecommerce/ProductInfoImpl.java
+++ java-grpc/src/main/java/ecommerce/ProductInfoImpl.java
@@ -13,26 +13,37 @@ public class ProductInfoImpl extends ProductInfoGrpc.ProductInfoImplBase {
 
     private final Map<String, Product> productMap = new HashMap<>();
 
+    /**
+     * @param request          : ProductInfoOuterClass.Product, 서비스 정의에 의해 생성됨
+     * @param responseObserver : 클라이언트에게 응답을 보내고, 스트림을 닫는 데 사용됨
+     */
     @Override
     public void addProduct(Product request, StreamObserver<ProductID> responseObserver) {
-        UUID uuid = UUID.randomUUID();
-        String randomUUIDString = uuid.toString();
-        request = request.toBuilder().setId(randomUUIDString).build();
+        String randomUUIDString = UUID.randomUUID().toString();
+        request = request.toBuilder()
+                         .setId(randomUUIDString)
+                         .build();
         productMap.put(randomUUIDString, request);
-        ProductInfoOuterClass.ProductID id = ProductInfoOuterClass.ProductID.newBuilder().setValue(randomUUIDString).build();
+        ProductID id = ProductID.newBuilder()
+                                .setValue(randomUUIDString)
+                                .build();
         responseObserver.onNext(id);
         responseObserver.onCompleted();
     }
 
+    /**
+     * @param request          : ProductInfoOuterClass.ProductID, 서비스 정의에 의해 생성됨
+     * @param responseObserver : 클라이언트에게 응답을 보내고, 스트림을 닫는 데 사용됨
+     */
     @Override
     public void getProduct(ProductID request, StreamObserver<Product> responseObserver) {
         String id = request.getValue();
         if (productMap.containsKey(id)) {
-            responseObserver.onNext((ProductInfoOuterClass.Product)productMap.get(id));
-            responseObserver.onCompleted();
+            responseObserver.onNext(productMap.get(id)); // 클라이언트에게 응답을 보냄
+            responseObserver.onCompleted(); // 스트림을 닫아 클라이언트 호출을 종료한다
         }
         else {
-            responseObserver.onError(new StatusException(Status.NOT_FOUND));
+            responseObserver.onError(new StatusException(Status.NOT_FOUND)); // 클라이언트에게 에러를 보낸다
         }
     }
 }
