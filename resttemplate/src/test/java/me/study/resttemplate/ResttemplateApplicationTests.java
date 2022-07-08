be0b130505835ae0e5b1507a7572c5883e105ec5
Jinseong Hwang <jinseong.dev@gmail.com>
2022-07-08 19:55:47 +0900

    feat: init project

---
+++ resttemplate/src/test/java/me/study/resttemplate/ResttemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package me.study.resttemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResttemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
