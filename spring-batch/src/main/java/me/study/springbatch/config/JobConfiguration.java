03d6223442dafed242ba11b2b6571182fa964719
jinseonghwang <eddy5360@naver.com>
2022-08-21 16:58:49 +0900

    feat: taskelt

--- spring-batch/src/main/java/me/study/springbatch/config/JobConfiguration.java
+++ spring-batch/src/main/java/me/study/springbatch/config/JobConfiguration.java
@@ -1,7 +1,6 @@
 package me.study.springbatch.config;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
@@ -10,37 +9,34 @@ import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Slf4j
 @RequiredArgsConstructor
 @Configuration
-public class HelloJobConfiguration {
+public class JobConfiguration {
 
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
 
     @Bean
-    public Job helloJob() {
-        return jobBuilderFactory.get("helloJob")
-                                .start(helloStep1())
-                                .next(helloStep2())
+    public Job job() {
+        return jobBuilderFactory.get("job")
+                                .start(step1())
+                                .next(step2())
                                 .build();
     }
 
-    @Bean
-    public Step helloStep1() {
-        return stepBuilderFactory.get("helloStep1")
+    private Step step1() {
+        return stepBuilderFactory.get("step1")
                                  .tasklet((contribution, chunkContext) -> {
-                                     log.info(">> Step 1 : Hello Spring Batch");
+                                     System.out.println("Step1 was excuted");
                                      return RepeatStatus.FINISHED;
                                  })
                                  .build();
     }
 
-    @Bean
-    public Step helloStep2() {
-        return stepBuilderFactory.get("helloStep2")
+    private Step step2() {
+        return stepBuilderFactory.get("step2")
                                  .tasklet((contribution, chunkContext) -> {
-                                     log.info(">> Step 2 : Hello Spring Batch");
+                                     System.out.println("Step2 was excuted");
                                      return RepeatStatus.FINISHED;
                                  })
                                  .build();
