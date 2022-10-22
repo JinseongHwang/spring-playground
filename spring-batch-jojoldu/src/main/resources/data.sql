8204541f86f58b060b7830413e468522a0fd371d
jinseonghwang <eddy5360@naver.com>
2022-10-22 19:16:03 +0900

    feat: JdbcBatchItemWriter 실습

--- spring-batch-jojoldu/src/main/resources/data.sql
+++ spring-batch-jojoldu/src/main/resources/data.sql
@@ -9,6 +9,15 @@ create table if not exists pay
     primary key (id)
 ) engine = InnoDB;
 
+create table if not exists pay2
+(
+    id           bigint not null auto_increment,
+    amount       bigint,
+    tx_name      varchar(255),
+    tx_date_time datetime,
+    primary key (id)
+) engine = InnoDB;
+
 insert ignore into pay (id, amount, tx_name, tx_date_time) VALUES (1, 1000, 'trade1', '2018-09-10 00:00:00');
 insert ignore into pay (id, amount, tx_name, tx_date_time) VALUES (2, 2000, 'trade2', '2018-09-10 00:00:00');
 insert ignore into pay (id, amount, tx_name, tx_date_time) VALUES (3, 3000, 'trade3', '2018-09-10 00:00:00');
