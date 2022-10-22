fc0b4f8ea2f237e524c0e66c32b38e6f7a2afb5d
jinseonghwang <eddy5360@naver.com>
2022-10-22 16:58:53 +0900

    feat: change package path

--- spring-batch-jojoldu/src/main/java/me/study/springbatchjojoldu/job/flow/scope/ScopeJobConfiguration.java
+++ spring-batch-jojoldu/src/main/java/me/study/springbatchjojoldu/job/flow/scope/ScopeJobConfiguration.java
@@ -1,4 +1,4 @@
-package me.study.springbatchjojoldu.job.scope;
+package me.study.springbatchjojoldu.job.flow.scope;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
--- spring-batch-jojoldu/src/main/java/me/study/springbatchjojoldu/job/flow/scope/SimpleJobTasklet.java
+++ spring-batch-jojoldu/src/main/java/me/study/springbatchjojoldu/job/flow/scope/SimpleJobTasklet.java
@@ -1,4 +1,4 @@
-package me.study.springbatchjojoldu.job.scope;
+package me.study.springbatchjojoldu.job.flow.scope;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.StepContribution;
