030ecd5944836dbfa481eeabe0bcfecc44a61b2f
jinseonghwang <eddy5360@naver.com>
2022-11-13 02:59:00 +0900

    feat: init project

---
+++ multiple-datasource/src/main/java/me/study/multipledatasource/MultipleDatasourceApplication.java
@@ -0,0 +1,13 @@
+package me.study.multipledatasource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultipleDatasourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultipleDatasourceApplication.class, args);
+    }
+
+}
