5202f3c47a299ccd7c14333d3d0072161967c44e
JinseongHwang <jinseong.dev@gmail.com>
2024-03-24 16:51:25 +0900

    init artillery project

---
+++ artillery/src/main/kotlin/me/study/artillery/ArtilleryApplication.kt
@@ -0,0 +1,11 @@
+package me.study.artillery
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ArtilleryApplication
+
+fun main(args: Array<String>) {
+    runApplication<ArtilleryApplication>(*args)
+}
---
+++ artillery/src/test/kotlin/me/study/artillery/ArtilleryApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.study.artillery
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ArtilleryApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
