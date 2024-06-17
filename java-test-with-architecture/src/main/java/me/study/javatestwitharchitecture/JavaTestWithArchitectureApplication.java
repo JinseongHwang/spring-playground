a7ca007055e5526c59fa4cd4c7840cf084a41af2
JinseongHwang <jinseong.dev@gmail.com>
2024-06-17 21:20:31 +0900

    init project

---
+++ java-test-with-architecture/src/main/java/me/study/javatestwitharchitecture/JavaTestWithArchitectureApplication.java
@@ -0,0 +1,13 @@
+package me.study.javatestwitharchitecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTestWithArchitectureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaTestWithArchitectureApplication.class, args);
+    }
+
+}
---
+++ java-test-with-architecture/src/test/java/me/study/javatestwitharchitecture/JavaTestWithArchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package me.study.javatestwitharchitecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaTestWithArchitectureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
