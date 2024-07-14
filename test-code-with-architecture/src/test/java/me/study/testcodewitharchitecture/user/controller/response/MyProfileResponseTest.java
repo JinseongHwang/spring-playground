2fa8dbbd6d92be8ed8943905efd4b582bc40110c
JinseongHwang <jinseong.dev@gmail.com>
2024-07-15 02:56:01 +0900

    MyProfileResponse 테스트 템플릿 추가

---
+++ test-code-with-architecture/src/test/java/me/study/testcodewitharchitecture/user/controller/response/MyProfileResponseTest.java
@@ -0,0 +1,15 @@
+package me.study.testcodewitharchitecture.user.controller.response;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MyProfileResponseTest {
+
+    @Test
+    public void User로_응답을_생성할_수_있다() {
+        // given
+        // when
+        // then
+    }
+}
