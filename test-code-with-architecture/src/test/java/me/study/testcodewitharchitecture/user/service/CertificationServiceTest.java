53f96265f539dc7a28837d99db7ca77cec822b9d
JinseongHwang <jinseong.dev@gmail.com>
2024-07-15 00:22:32 +0900

    CertificationService 테스트 추가

---
+++ test-code-with-architecture/src/test/java/me/study/testcodewitharchitecture/user/service/CertificationServiceTest.java
@@ -0,0 +1,28 @@
+package me.study.testcodewitharchitecture.user.service;
+
+import me.study.testcodewitharchitecture.mock.FakeMailSender;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CertificationServiceTest {
+
+    @Test
+    void 이메일과_컨텐츠가_제대로_만들어져서_보내지는지_테스트() {
+        // given
+        FakeMailSender mailSender = new FakeMailSender();
+        CertificationService certificationService = new CertificationService(mailSender);
+
+        // when
+        certificationService.send("abc@example.com", 1, "1234");
+
+        // then
+        assertThat(mailSender.email).isEqualTo("abc@example.com");
+        assertThat(mailSender.title).isEqualTo("Please certify your email address");
+        assertThat(mailSender.content).isEqualTo(
+                "Please click the following link to certify your email address: " +
+                        "http://localhost:8080/api/users/1/verify?certificationCode=1234"
+        );
+    }
+}
